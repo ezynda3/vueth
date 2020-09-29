5605f32e2934b0d476ee9ca12eabe3413a74665a
Ed Zynda <ezynda3@gmail.com>
2020-09-29 14:31:14 +0800

    add vue-tailwind for complex component behaviour

--- packages/vue-app/src/components/Wallet.vue
+++ packages/vue-app/src/components/Wallet.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="flex">
-    <svg class="w-10 h-10 text-blue-600" viewBox="64 64 896 896" focusable="false" data-icon="wallet" width="1em" height="1em" fill="currentColor" aria-hidden="true" style="transform: rotate(-90deg);">
+    <svg
+      @click="showModal = !showModal"
+      class="w-10 h-10 text-blue-600 cursor-pointer"
+      viewBox="64 64 896 896"
+      focusable="false"
+      data-icon="wallet"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      aria-hidden="true"
+      style="transform: rotate(-90deg);"
+    >
       <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zm-40 464H528V448h312v128zm0 264H184V184h656v200H496c-17.7 0-32 14.3-32 32v192c0 17.7 14.3 32 32 32h344v200zM580 512a40 40 0 1080 0 40 40 0 10-80 0z"></path>
     </svg>
+    <t-modal
+      v-model="showModal"
+      header="Title of the modal"
+    >
+      TODO: Create Wallet UI
+    <template v-slot:footer>
+      <div class="flex justify-between">
+        <t-button type="button">
+          Cancel
+        </t-button>
+        <t-button type="button">
+          Ok
+        </t-button>
+      </div>
+    </template>
+  </t-modal>
   </div>
 </template>
 
@@ -10,5 +37,7 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Wallet extends Vue {}
+export default class Wallet extends Vue {
+  showModal = false
+}
 </script>
--- packages/vue-app/src/main.ts
+++ packages/vue-app/src/main.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VueTailwind from 'vue-tailwind'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import tailwindSettings from './vue-tailwind-settings'
 
 Vue.config.productionTip = false
+Vue.use(VueTailwind, tailwindSettings)
 
 new Vue({
   router,

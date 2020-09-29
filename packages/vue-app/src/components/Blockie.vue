cb653c3c0058d6934b02e2800672d812f1e0198d
Ed Zynda <ezynda3@gmail.com>
2020-09-29 14:50:03 +0800

    extract blockie into separate component

---
+++ packages/vue-app/src/components/Blockie.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+    <img :src="blockieSrc" class="w-10 h-10"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import * as blockies from 'blockies-ts'
+
+@Component
+export default class Blockie extends Vue {
+  @Prop({ default: '' }) readonly address: string
+
+  copying = false
+
+  get blockieSrc(): string {
+    return blockies.create({ seed: this.address }).toDataURL()
+  }
+
+  async copy() {
+    this.copying = true
+    setTimeout(() => { this.copying = false }, 1000)
+  }
+}
+</script>

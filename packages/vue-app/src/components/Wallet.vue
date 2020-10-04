d03cc023049a2338eda3a1e978202048256a20c8
Ed Zynda <ezynda3@gmail.com>
2020-10-04 16:39:53 +0800

    rotate send icon

--- packages/vue-app/src/components/Wallet.vue
+++ packages/vue-app/src/components/Wallet.vue
@@ -38,7 +38,7 @@
               {{ showQR ? 'Hide' : 'Receive' }}
             </t-button>
             <t-button type="button" variant="simple" :disabled="!validAddress">
-              <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg>
+              <svg class="w-6 h-6 mr-1 transform rotate-90" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg>
               Send
             </t-button>
           </div>

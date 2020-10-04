1fa4c5af8139f73673b5efa874e57fc18136d44f
Ed Zynda <ezynda3@gmail.com>
2020-10-04 16:34:36 +0800

    show and hide qr code

--- packages/vue-app/src/components/Wallet.vue
+++ packages/vue-app/src/components/Wallet.vue
@@ -23,20 +23,25 @@
           <Balance class="mr-5"/>
         </div>
       </template>
-      <div class="mt-5">
+      <div v-if="showQR" class="m-5">
+        <qrcode-vue :value="address" level="H" render-as="svg" size="450" />
+      </div>
+      <div v-else class="mt-5">
         <AddressInput v-model="toAddress"/>
         <EtherInput/>
       </div>
       <template v-slot:footer>
-        <div class="flex justify-end">
-          <t-button type="button" class="mr-1" variant="simple">
-            <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path></svg>
-            Receive
-          </t-button>
-          <t-button type="button" variant="simple" :disabled="!validAddress">
-            <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg>
-            Send
-          </t-button>
+        <div>
+          <div class="flex justify-end">
+            <t-button type="button" class="mr-1" variant="simple" @click="showQR = !showQR">
+              <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path></svg>
+              {{ showQR ? 'Hide' : 'Receive' }}
+            </t-button>
+            <t-button type="button" variant="simple" :disabled="!validAddress">
+              <svg class="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg>
+              Send
+            </t-button>
+          </div>
         </div>
     </template>
   </t-modal>
@@ -46,6 +51,7 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { isAddress } from '@ethersproject/address'
+import QrcodeVue from 'qrcode.vue'
 import Balance from './Balance.vue'
 import Address from './Address.vue'
 import AddressInput from './AddressInput.vue'
@@ -57,6 +63,7 @@ import EtherInput from './EtherInput.vue'
     AddressInput,
     Balance,
     EtherInput,
+    QrcodeVue,
   },
 })
 export default class Wallet extends Vue {
@@ -65,6 +72,8 @@ export default class Wallet extends Vue {
   toAddress = ''
 
   showModal = false
+
+  showQR = false
 
   get validAddress() {
     return isAddress(this.toAddress)
